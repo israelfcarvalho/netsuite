c76ad18cb94ee4ffd53f66c475b00772a866c709
Israel <israel.facanha@gmail.com>
2025-01-05 22:27:24 -0300

    Update web app name from web to storage-sync

--- apps/storage-sync/app/page.tsx
+++ apps/storage-sync/app/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@workspace/ui/components/button"
+import { Command, CommandInput } from "@workspace/ui/components/command"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Command>
+          <CommandInput placeholder="Enter your name" />
+        </Command>
       </div>
     </div>
   )
