e698f3fec9e83480e4674c858a8c0a056050aa53
Israel <israel.facanha@gmail.com>
2025-01-10 18:23:53 -0300

    Add custom basePath to nextConfig with webpack

--- apps/storage-sync/next.config.mjs
+++ apps/storage-sync/next.config.mjs
@@ -1,7 +1,17 @@
+const buildForNetsuite = true
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  output: 'export'
+  output: 'export',
+  webpack(config){
+    
+    if(buildForNetsuite){
+      config.output.publicPath = '/app/site/hosting/scriptlet.nl?script=customscript_sm_st_saved_search_sett_ui&deploy=customdeploy_sm_st_saved_search_sett_ui&resource=/_next/'
+    }
+
+    return config
+  }
 }
 
 export default nextConfig
