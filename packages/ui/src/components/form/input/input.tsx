6cb9542f5ba4c74ad7d5d74067c6b706fa263fa9
Israel <israel.facanha@gmail.com>
2025-01-31 09:35:50 -0300

    Upgrade input component

--- packages/ui/src/components/form/input/input.tsx
+++ packages/ui/src/components/form/input/input.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { FormControl, FormField, FormLabel } from '@radix-ui/react-form'
 import { InputProps } from "./input.types";
+import { cn } from "@workspace/ui/lib/utils";
 
 export const Input: React.FC<InputProps> = ({
     name,
     label, 
     onChange, 
     value,
-    required = false
+    required = false,
+    labeless,
+    variant
 }) => {
     return (
         <FormField name={name}>
+            {!labeless && (
             <FormLabel
                 className="font-sans text-xs text-input-label font-semibold"
             >
@@ -18,10 +22,15 @@ export const Input: React.FC<InputProps> = ({
                 
                 {required && <span className="ml-[2px] text-light-danger-120 text-base">*</span>}
             </FormLabel>
+            )}
             <FormControl
-                className="border-input-border border rounded-md w-full min-h-8 text-sm px-2 outline-input-border-selected"
-                type="text" 
-                name={name} 
+                aria-label={!!labeless ? label : undefined}
+                className={cn(
+                    "border-input-border border rounded-md w-full min-h-8 text-sm px-2 outline-input-border-selected",
+                    {'border-none': variant === 'outline' }
+                )}
+                type="text"
+                name={name}
                 value={value}
                 onChange={(event) => onChange(event.target.value)}
             />
--- packages/ui/src/components/form/input/input.types.ts
+++ packages/ui/src/components/form/input/input.types.ts
@@ -1,7 +1,11 @@
+type InputVariant = 'outline'
+
 export interface InputProps {
     name: string
     label: string
     value?: string
     required?: boolean
     onChange: (value: string) => void
+    labeless?:boolean
+    variant?: InputVariant
 }
