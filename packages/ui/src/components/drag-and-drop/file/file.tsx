4f6b0f03b6c037911963719942e84f5a6bdde264
Israel <israel.facanha@gmail.com>
2025-02-01 22:33:26 -0300

    Add button to delete all files on component dragAndDropFiles

--- packages/ui/src/components/drag-and-drop/file/file.tsx
+++ packages/ui/src/components/drag-and-drop/file/file.tsx
@@ -41,31 +41,34 @@ export const DropZoneFile: React.FC = () => {
         })
     }
 
+    const deleteAllFiles = () => {
+        setFiles(new Map())
+    }
+
     return (
-        <div className="h-36">
-            <DropZoneBase
-                className={`w-fit max-w-full p-4 data-[state=dragging-over]:bg-light-brand-40 hover:bg-light-brand-40 grid grid-cols-[minmax(300px,auto)_1fr] gap-4 items-center border-dotted border-[3px] border-light-neutral-70`}
-                onDragOver={handleDragOver}
-                onDrop={handleDrop}
-            >
-                <div className="grid gap-y-1 justify-center">
-                    <p>Drop your files here or...</p>
-                    <Button onClick={() => inputFileRef.current?.click()}>Select file(s)</Button>
-                    <input ref={inputFileRef} className="hidden" aria-label="select file" type="file" multiple onChange={handleSelectFile}/>
-                </div>
-
-                <ul className="h-full flex justify-center flex-col flex-wrap gap-y-1 gap-x-10 overflow-auto w-fit text-light-neutral-100">
-                    {filesArray.map(file => (
-                        <li className="flex gap-2 items-center justify-between" key={file.name}>
-                            {file.name}
-
-                            <Button className="p-0 h-fit" variant="ghost" onClick={() => deleteFile(file)}>
-                                <Trash2 className="size-4"/>
-                            </Button>
-                        </li>
-                    ))}
-                </ul>
-            </DropZoneBase>
-        </div>
+        <DropZoneBase
+            className={`min-h-36 w-fit p-4 data-[state=dragging-over]:bg-light-brand-40 hover:bg-light-brand-40 grid grid-cols-[minmax(300px,auto)_1fr] gap-4 items-center border-dotted border-[3px] border-light-neutral-70`}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+        >
+            <div className="grid gap-y-2 justify-center">
+                <p>Drop your files here or...</p>
+                <Button onClick={() => inputFileRef.current?.click()}>Select file(s)</Button>
+                {!!filesArray.length && (
+                    <Button variant="outline" onClick={deleteAllFiles}>Delete all file(s)</Button>
+                )}
+                <input ref={inputFileRef} className="hidden" aria-label="select file" type="file" multiple onChange={handleSelectFile}/>
+            </div>
+            <ul className="h-full grid grid-flow-col auto-cols-max grid-rows-5 gap-y-1 gap-x-10 overflow-auto w-full text-light-neutral-100">
+                {filesArray.map(file => (
+                    <li className="flex gap-2 items-center justify-between" key={file.name}>
+                        {file.name}
+                        <Button className="p-0 h-fit" variant="ghost" onClick={() => deleteFile(file)}>
+                            <Trash2 className="size-4"/>
+                        </Button>
+                    </li>
+                ))}
+            </ul>
+        </DropZoneBase>
     )
 }
