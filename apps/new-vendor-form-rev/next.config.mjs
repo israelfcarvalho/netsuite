875df36954408c9988f63632589d2b58a4032f0a
Israel <israel.facanha@gmail.com>
2025-02-11 01:45:19 -0300

    Remove uneccessary code for nextjs config
    build public path in production now is handled
    inside ainfraestructure project that is called by build script

--- apps/new-vendor-form-rev/next.config.mjs
+++ apps/new-vendor-form-rev/next.config.mjs
@@ -1,19 +1,7 @@
-const buildForNetsuite = false
-
-console.log('on config', process.env.NODE_ENV)
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  output: 'export',
-  webpack(config){
-    
-    if(buildForNetsuite){
-      config.output.publicPath = '/app/site/hosting/scriptlet.nl?script=customscript_sp_st_external_framework_ui&deploy=customdeploy_sp_st_external_framework_ui&resource=/_next/'
-    }
-
-    return config
-  }
+  output: 'export'
 }
 
 export default nextConfig
--- apps/storage-sync/next.config.mjs
+++ apps/storage-sync/next.config.mjs
@@ -1,19 +1,7 @@
-const buildForNetsuite = false
-
-console.log('on config', process.env.NODE_ENV)
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  output: 'export',
-  webpack(config){
-    
-    if(buildForNetsuite){
-      config.output.publicPath = '/app/site/hosting/scriptlet.nl?script=customscript_sm_st_saved_search_sett_ui&deploy=customdeploy_sm_st_saved_search_sett_ui&resource=/_next/'
-    }
-
-    return config
-  }
+  output: 'export'
 }
 
 export default nextConfig
